0d135a5cfdc80c092173314326ba29dd7fa8f108
scnace <scbizu@gmail.com>
2020-02-11 00:31:00 +0800

    all: refactor

--- tg.go
+++ tg.go
@@ -16,7 +16,6 @@ import (
 type Bot struct {
 	bot         *tgbotapi.BotAPI
 	isDebugMode bool
-	msgs        map[MSGType]<-chan tgbotapi.Update
 }
 
 func NewBot(isDebugMode bool) (*Bot, error) {
@@ -32,32 +31,21 @@ func NewBot(isDebugMode bool) (*Bot, error) {
 		tgConn.Debug = true
 		logrus.Infof("bot auth passed as %s", tgConn.Self.UserName)
 	}
-	bot.msgs = make(map[MSGType]<-chan tgbotapi.Update)
 	return bot, nil
 }
 
-type MSGType string
-
-const (
-	MSGTypeText   MSGType = "text"
-	MSGTypeInline MSGType = "inline"
-)
-
-func (b *Bot) RegisterMsgChannel(cnames ...MSGType) {
-	for _, name := range cnames {
-		b.msgs[name] = make(chan tgbotapi.Update, b.bot.Buffer)
-	}
-}
-
 func listenWebhook() {
-
 	port := fmt.Sprintf(":%s", listenPort)
 	if err := http.ListenAndServe(port, nil); err != nil {
 		panic(err)
 	}
 }
 
-func (b *Bot) RegisterWebhook() {
+func (b *Bot) RegisterWebhook(
+	resHandler func(updateMsg tgbotapi.Update) ([]interface{}, error),
+	OnChosenHandler func(*tgbotapi.ChosenInlineResult, *tgbotapi.BotAPI) error,
+	plugins ...plugin.MessagePlugin,
+) {
 
 	b.setWebhookOnce()
 	router := fmt.Sprintf("/tg/%s", token)
@@ -68,12 +56,14 @@ func (b *Bot) RegisterWebhook() {
 
 		var update tgbotapi.Update
 		json.Unmarshal(bytes, &update)
-		// fan out
-		for name := range b.msgs {
-			uChan := make(chan tgbotapi.Update, b.bot.Buffer)
-			uChan <- update
-			b.msgs[name] = uChan
+
+		if err := b.serveBotUpdateMessage(update, plugins...); err != nil {
+			logrus.Errorf("mytg: %q", err)
 		}
+		if err := b.serveInlineMode(update, resHandler, OnChosenHandler); err != nil {
+			logrus.Errorf("mytg: %q", err)
+		}
+
 	})
 
 	go listenWebhook()
@@ -104,70 +94,63 @@ func (b *Bot) setWebhookOnce() {
 	})
 }
 
-func (b *Bot) ServeInlineMode(
+func (b *Bot) serveInlineMode(msg tgbotapi.Update,
 	resHandler func(updateMsg tgbotapi.Update) ([]interface{}, error),
 	OnChosenHandler func(*tgbotapi.ChosenInlineResult, *tgbotapi.BotAPI) error) error {
-	msgs := b.msgs[MSGTypeInline]
-	for msg := range msgs {
-		if msg.InlineQuery == nil {
-			continue
-		}
-		if msg.ChosenInlineResult != nil &&
-			OnChosenHandler != nil {
-			if err := OnChosenHandler(msg.ChosenInlineResult, b.bot); err != nil {
-				logrus.Errorf("inline mode: %q", err)
-				continue
-			}
-			return nil
-		}
-		r, err := resHandler(msg)
-		if err != nil {
-			continue
-		}
-		config := tgbotapi.InlineConfig{
-			InlineQueryID: msg.InlineQuery.ID,
-			Results:       r,
-			IsPersonal:    true,
-		}
-		if _, err := b.bot.AnswerInlineQuery(config); err != nil {
+	if msg.InlineQuery == nil {
+		return nil
+	}
+	if msg.ChosenInlineResult != nil &&
+		OnChosenHandler != nil {
+		if err := OnChosenHandler(msg.ChosenInlineResult, b.bot); err != nil {
+			logrus.Errorf("inline mode: %q", err)
 			return err
 		}
+		return nil
+	}
+	r, err := resHandler(msg)
+	if err != nil {
+		return err
+	}
+	config := tgbotapi.InlineConfig{
+		InlineQueryID: msg.InlineQuery.ID,
+		Results:       r,
+		IsPersonal:    true,
+	}
+	if _, err := b.bot.AnswerInlineQuery(config); err != nil {
+		return err
 	}
 	return nil
 }
 
-func (b *Bot) ServeBotUpdateMessage(plugins ...plugin.MessagePlugin) error {
-	msgs := b.msgs[MSGTypeText]
-	logrus.Debugf("msg in channel:%d", len(msgs))
-	for update := range msgs {
-		logrus.Debugf("[raw msg]:%#v\n", update)
+func (b *Bot) serveBotUpdateMessage(update tgbotapi.Update, plugins ...plugin.MessagePlugin) error {
+	logrus.Debugf("[raw msg]:%#v\n", update)
 
-		if update.Message == nil {
+	if update.Message == nil {
+		return nil
+	}
+
+	var config tgbotapi.Chattable
+
+	for _, p := range plugins {
+		var err error
+		config, err = p.HandleMessage(
+			update.Message,
+		)
+
+		if errors.Is(err, plugin.ErrMessageNotMatched) {
 			continue
 		}
 
-		var config tgbotapi.Chattable
-
-		for _, p := range plugins {
-			var err error
-			config, err = p.HandleMessage(
-				update.Message,
-			)
-
-			if errors.Is(err, plugin.ErrMessageNotMatched) {
-				continue
-			}
-
-			if err != nil {
-				logrus.Errorf("plugin: %q", err)
-				continue
-			}
-		}
-
-		if _, err := b.bot.Send(config); err != nil {
-			logrus.Errorf("mytg: send message: %q", err)
+		if err != nil {
+			logrus.Errorf("plugin: %q", err)
 			continue
 		}
+	}
+
+	if _, err := b.bot.Send(config); err != nil {
+		logrus.Errorf("mytg: send message: %q", err)
+		return err
 	}
 	return nil
 }
